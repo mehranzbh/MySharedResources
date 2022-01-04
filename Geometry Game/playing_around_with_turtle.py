6a3fb8df961acd79417c80ea11f96ada0b854683
Mehran <66390104+mehranzbh@users.noreply.github.com>
2022-01-03 23:35:46 -0500

    Add files via upload

---
+++ Geometry Game/playing_around_with_turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+myturtle=turtle.Turtle()
+
+myturtle.penup()
+myturtle.goto(100,100)
+
+myturtle.pendown()
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
