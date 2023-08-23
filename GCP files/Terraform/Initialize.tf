48611359b4dbe7b35c5561dc47e47c9b184a8242
Mehran <66390104+mehranzbh@users.noreply.github.com>
2023-08-23 18:01:53 -0400

    Update Initialize.tf

--- GCP files/Terraform/Initialize.tf
+++ GCP files/Terraform/Initialize.tf
@@ -1,3 +1,5 @@
+#the file name should be changed to main.tf after plugins has been installed (provider plugins - GCP plugins), then the terraform plan command with the main.tf revised file (which includes further configuration) should be run.
+
 terraform {
   required_providers {
     google = {
