6a38aa40dae23fc5d7a8520d22ecb8e1c678e09d
Mehran <66390104+mehranzbh@users.noreply.github.com>
2023-08-24 20:45:12 -0400

    Update instance.tf

--- GCP files/Terraform/advanced/instance.tf
+++ GCP files/Terraform/advanced/instance.tf
@@ -1,4 +1,7 @@
-resource google_compute_instance "vm_instance" {
+ resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+ resource google_compute_instance "vm_instance" {
 name         = "${var.instance_name}"
 zone         = "${var.instance_zone}"
 machine_type = "${var.instance_type}"
@@ -11,6 +14,7 @@ boot_disk {
     network = "default"
     access_config {
       # Allocate a one-to-one NAT IP to the instance
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
 }
