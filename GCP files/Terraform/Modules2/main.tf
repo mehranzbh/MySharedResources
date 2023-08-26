2cf1083e1aa5413807d6993e36fdddb91e69673a
Mehran <66390104+mehranzbh@users.noreply.github.com>
2023-08-26 00:39:08 -0400

    Update main.tf

--- GCP files/Terraform/Modules2/main.tf
+++ GCP files/Terraform/Modules2/main.tf
@@ -1,15 +1,9 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "${var.instance_name}"
-  zone         = "${var.instance_zone}"
-  machine_type = "${var.instance_type}"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      }
-  }
-  network_interface {
-    network = "${var.instance_network}"
-    access_config {
-      # Allocate a one-to-one NAT IP to the instance
-    }
-  }
+module "dev_network" {
+  source = "./network"
+  network_name= "my-network-1"
+}
+
+module "dev_network" {
+  source = "./network"
+  network_name= "my-network-2"
+}
